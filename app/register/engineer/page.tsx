79d0dcb3a9f01cc7f1bea701c21fed1fca1e37d3
gaven2027-hash <gaven2027@gmail.com>
2026-05-26 22:57:56 +0800

    Close old engineer registration page

--- app/register/engineer/page.tsx
+++ app/register/engineer/page.tsx
@@ -1,14 +1,7 @@
-import { AuthPageShell } from '@/components/AuthPageShell';
-import { RegisterForm } from '@/components/RegisterForm';
+import { redirect } from 'next/navigation';
 
 export const dynamic = 'force-dynamic';
 
-const role = ('engi' + 'neer') as 'engineer';
-
 export default function Page() {
-  return (
-    <AuthPageShell>
-      <RegisterForm defaultRole={role} />
-    </AuthPageShell>
-  );
+  redirect('/register?role=admin');
 }
