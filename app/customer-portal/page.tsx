42923a455f9dadd21e77b7c5c01bc6bd2e37fcfa
gaven2027-hash <gaven2027@gmail.com>
2026-05-27 22:53:57 +0800

    Wire customer portal request workspace into portal page

--- app/customer-portal/page.tsx
+++ app/customer-portal/page.tsx
@@ -1,6 +1,7 @@
 export const dynamic = 'force-dynamic';
 
 import { Customer360 } from '@/components/Customer360';
+import { CustomerPortalRequestWorkspace } from '@/components/CustomerPortalRequestWorkspace';
 import { CustomerPortalDataLoop } from '@/components/PortalDataLoop';
 import { CustomerPortalAnchors, PortalShell } from '@/components/PortalShell';
 
@@ -8,6 +9,7 @@ export default function Page() {
   return (
     <PortalShell type="customer">
       <CustomerPortalDataLoop />
+      <CustomerPortalRequestWorkspace />
       <Customer360 />
       <CustomerPortalAnchors />
     </PortalShell>
