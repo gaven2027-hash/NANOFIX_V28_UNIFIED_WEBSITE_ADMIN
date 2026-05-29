4a1e8f447daf61d7639356793d31d19411549242
gaven2027-hash <gaven2027@gmail.com>
2026-05-29 15:32:10 +0800

    Use customer portal shell dashboard on portal home

--- app/customer-portal/page.tsx
+++ app/customer-portal/page.tsx
@@ -1,17 +1,22 @@
 export const dynamic = 'force-dynamic';
 
 import { Customer360 } from '@/components/Customer360';
+import { CustomerPortalDashboard } from '@/components/CustomerPortalDashboard';
 import { CustomerPortalRequestWorkspace } from '@/components/CustomerPortalRequestWorkspace';
+import { CustomerPortalShell } from '@/components/CustomerPortalShell';
 import { CustomerPortalDataLoop } from '@/components/PortalDataLoop';
-import { CustomerPortalAnchors, PortalShell } from '@/components/PortalShell';
+import { CustomerPortalAnchors } from '@/components/PortalShell';
 
 export default function Page() {
   return (
-    <PortalShell type="customer">
-      <CustomerPortalDataLoop />
-      <CustomerPortalRequestWorkspace />
-      <Customer360 />
-      <CustomerPortalAnchors />
-    </PortalShell>
+    <CustomerPortalShell>
+      <div className="space-y-6">
+        <CustomerPortalDashboard />
+        <CustomerPortalDataLoop />
+        <CustomerPortalRequestWorkspace />
+        <Customer360 />
+        <CustomerPortalAnchors />
+      </div>
+    </CustomerPortalShell>
   );
 }
