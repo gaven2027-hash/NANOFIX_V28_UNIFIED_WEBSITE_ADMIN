25307fb5ad7c0836b17b95b6b24dfaa328ad5eb4
gaven2027-hash <gaven2027@gmail.com>
2026-06-04 17:55:43 +0800

    V28.4.2 hide module diagnostics from daily admin workspaces

--- components/MenuAnchorSections.tsx
+++ components/MenuAnchorSections.tsx
@@ -1,5 +1,11 @@
 import { AdminSubmoduleWorkspace } from './AdminSubmoduleWorkspace';
 
+const DIAGNOSTIC_ROUTES = new Set(['/system-settings']);
+
 export function MenuAnchorSections({ route }: { route: string }) {
+  if (!DIAGNOSTIC_ROUTES.has(route)) {
+    return null;
+  }
+
   return <AdminSubmoduleWorkspace route={route} />;
 }
